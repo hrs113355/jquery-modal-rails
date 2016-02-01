0973a57d288c799cfbc4706832b63cc1492698d3
hrs <hrs113355@gmail.com>
2016-02-01 17:11:34 +0800

    add image and font assets under vendor/assets/ to precompile list.

--- lib/jquery/modal/rails/engine.rb
+++ lib/jquery/modal/rails/engine.rb
@@ -6,6 +6,12 @@ module Jquery
           g.test_framework :rspec
           g.integration_tool :rspec
         end
+
+        initializer 'jquery-modal-rails.assets.precompile' do |app|
+          app.config.assets.precompile << Proc.new do |path, fname|
+            fname =~ /vendor\/assets\/(images|fonts)\/jquery-modal-rails/ && !%w(.js .css).include?(File.extname(path))
+          end
+        end
       end
     end
   end
